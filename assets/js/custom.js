447a99df109ced377a89778bfe23fbc50ff42fe9
Md. Abuhorayra <horayra5@gmail.com>
2022-09-02 07:42:51 +0600

    multiple section added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,81 @@
+// jQuery
+jQuery(document).ready(function ($) {
+
+    // magnific popPup
+    $(".video-play").magnificPopup({
+        type: 'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>',
+
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+
+                    id: 'v=',
+
+                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                },
+                vimeo: {
+                    index: 'vimeo.com/',
+                    id: '/',
+                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                },
+                gmaps: {
+                    index: '//maps.google.',
+                    src: '%id%&output=embed'
+                }
+
+
+            },
+
+            srcAction: 'iframe_src',
+        }
+    });
+
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        dots: false,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+
+
+
+});
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
-    // add padding top to show content behind navbar
-    navbar_height = document.querySelector('.navbar').offsetHeight;
-    document.body.style.paddingTop = navbar_height + 'px';
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
 }); 
